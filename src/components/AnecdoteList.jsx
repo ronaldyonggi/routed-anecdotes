d838bde12597ce1aebf80d94c30cbbe12ff131a0
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-28 18:01:45 +0800

    7.2 mapping anecdote include Links

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom"
+
 const AnecdoteList = ({ anecdotes }) => (
     <div>
         <h2>Anecdotes</h2>
         <ul>
-            {anecdotes.map(a => <li key={a.id}>{a.content}</li>)}
+            {anecdotes.map(a => 
+            <li key={a.id}>
+                <Link to={`/anecdotes/${a.id}`}> {a.content}</Link>
+            </li>
+            )}
         </ul>
     </div>
 )
