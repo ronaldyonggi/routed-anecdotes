a01ff0292e35c8372112c559ece27e9e24c8f912
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-29 21:02:36 +0800

    7.4 input fields now use custom useField hook

--- src/pages/CreateNew.jsx
+++ src/pages/CreateNew.jsx
@@ -1,25 +1,25 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useNotificationDispatch } from "../NotificationContext"
+import { useField } from "../hooks"
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const content = useField('text')
+    const author = useField('text')
+    const info = useField('text')
     const navigate = useNavigate()
     const dispatch = useNotificationDispatch()
 
     const handleSubmit = e => {
         e.preventDefault()
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         })
         dispatch({
             type: 'set',
-            payload: `a new anecdote ${content} is created!`
+            payload: `a new anecdote ${content.value} is created!`
         })
         setTimeout(() => {
             dispatch({
@@ -36,17 +36,18 @@ const CreateNew = (props) => {
             <form onSubmit={handleSubmit}>
                 <div>
                     content
-                    <input name="content" value={content} onChange={e => setContent(e.target.value)}/>
+                    {/* <input name="content" value={content} onChange={e => setContent(e.target.value)}/> */}
+                    <input name="content" {...content}/>
                 </div>
 
                 <div>
                     author
-                    <input name="author" value={author} onChange={e => setAuthor(e.target.value)}/>
+                    <input name="author" {...author}/>
                 </div>
 
                 <div>
                     url for more info
-                    <input name="info" value={info} onChange={e => setInfo(e.target.value)}/>
+                    <input name="info" {...info}/>
                 </div>
                 <button>create</button>
             </form>
