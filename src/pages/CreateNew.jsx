16abf2875be641c40da6815b75fa8e0405de6e83
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-01 09:37:22 +0800

    7.5 add button to reset fields

--- src/pages/CreateNew.jsx
+++ src/pages/CreateNew.jsx
@@ -30,6 +30,13 @@ const CreateNew = (props) => {
         navigate('/')
     }
 
+    const handleReset = e => {
+        e.preventDefault()
+        content.reset()
+        author.reset()
+        info.reset()
+    }
+
     return (
         <div>
             <h2>Create a New Anecdote</h2>
@@ -50,6 +57,7 @@ const CreateNew = (props) => {
                     <input name="info" {...info}/>
                 </div>
                 <button>create</button>
+                <button onClick={handleReset}>reset</button>
             </form>
         </div>
     )
